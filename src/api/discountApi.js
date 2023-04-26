fa4b5cd2958b3b7f68925a1f417f296ebe1691fe
Tala Hamad <128089053+talahamad1@users.noreply.github.com>
2023-04-26 17:23:49 +0300

    AR-55 resolve commints

--- src/api/discountApi.js
+++ src/api/discountApi.js
@@ -3,40 +3,60 @@ import axiosInstance from '../utils/axiosUtils';
 export const getDiscounts = async () => {
     try {
         const response = await axiosInstance.get('/discounts');
-        return response.data;
-    } catch (err) {
-        console.log("get discounts err :", err);
+        if (response.status === 200) {
+            return response.data;
+        }
+        throw new Error('Failed to fetch discounts');
+    } catch (error) {
+        console.error('Error fetching discounts:', error);
+        throw error;
     }
 };
 export const getDiscountById = async (id) => {
     try {
         const response = await axiosInstance.get(`/discount/${id}`);
-        return response.data;
-    } catch (err) {
-        console.log("get discount by id err :", err);
+        if (response.status === 200) {
+            return response.data;
+        }
+        throw new Error(`Failed to fetch discount with ID ${id}`);
+    } catch (error) {
+        console.error(`Error fetching discount with ID ${id}:`, error);
+        throw error;
     }
 };
 export const createDiscount = async (discountData) => {
     try {
         const response = await axiosInstance.post('/discount', discountData);
-        return response.data;
-    } catch (err) {
-        console.log("create discount err :", err);
+        if (response.status === 201) {
+            return response.data;
+        }
+        throw new Error('Failed to create discount');
+    } catch (error) {
+        console.error('Error creating discount:', error);
+        throw error;
     }
 };
 export const updateById = async (id, discountData) => {
     try {
         const response = await axiosInstance.put(`/discount/${id}`, discountData);
-        return response.data;
-    } catch (err) {
-        console.log("update discount err :", err);
+        if (response.status === 200) {
+            return response.data;
+        }
+        throw new Error(`Failed to update discount with ID ${id}`);
+    } catch (error) {
+        console.error(`Error updating discount with ID ${id}:`, error);
+        throw error;
     }
 };
 export const deleteDiscount = async (id) => {
     try {
         const response = await axiosInstance.delete(`/discount/${id}`);
-        return response.data;
-    } catch (err) {
-        console.log("delete discount  err :", err);
+        if (response.status === 200) {
+            return response.data;
+        }
+        throw new Error(`Failed to delete discount with ID ${id}`);
+    } catch (error) {
+        console.error(`Error deleting discount with ID ${id}:`, error);
+        throw error;
     }
 };
