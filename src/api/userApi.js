bbc4c280657aff83d04b7c9bdd8bf65135fe6b90
Momena-zaghal <76658126+Momena-zaghal@users.noreply.github.com>
2023-05-01 16:55:03 +0300

    AR-57-Create-the-Apis-for-Users-model

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -2,55 +2,79 @@ import axiosInstance from "../utils/axiosUtils";
 
 export const getUserById = async (id) => {
     try {
-        const response = await axiosInstance.get('/user/${id}');
-        return response.data;
+        const response = await axiosInstance.get(`/user/${id}`);
+        if (response.status === 200) {
+            return response.data;
+        }
+        throw new Error(`Failed to fetch user with ID ${id}`);
     }
     catch (error) {
         console.log("get user by id error:", error);
+        throw error;
     }
 };
 export const createUser = async (userData) => {
     try {
         const response = await axiosInstance.post('/user', userData);
-        return response.data;
+        if (response.status === 200) {
+            return response.data;
+        }
+        throw new Error('Failed to create user');
     }
     catch (error) {
         console.log("create user error:", error);
+        throw error;
     }
 };
 export const updateUser = async (id, userData) => {
     try {
-        const response = await axiosInstance.put('/user/${id}', userData);
-        return response.data;
+        const response = await axiosInstance.put(`/user/${id}`, userData);
+        if (response.status === 200) {
+            return response.data;
+        }
+        throw new Error(`Failed to update user with ID ${id}`);
     }
     catch (error) {
         console.log("update user error:", error);
+        throw error;
     }
 };
 export const removeUser = async (id) => {
     try {
-        const response = await axiosInstance.delete('/user/${id}');
-        return response.data;
+        const response = await axiosInstance.delete(`/user/${id}`);
+        if (response.status === 204) {
+            return response.data;
+        }
+        throw new Error(`Failed to remove user with ID ${id}`);
     }
     catch (error) {
         console.log("remove user error:", error);
+        throw error;
     }
 };
 export const login = async (id) => {
     try {
-        const response = await axiosInstance.post('/user/login');
-        return response.data;
+        const response = await axiosInstance.post(`/user/${id}`);
+        if (response.status === 200) {
+            return response.data;
+        }
+        throw new Error('Failed to login');
     }
     catch (error) {
         console.log("login error:", error);
+        throw error;
     }
 };
 export const getCurrentUser = async (id) => {
     try {
-        const response = await axiosInstance.get('/user/${id}');
-        return response.data;
+        const response = await axiosInstance.get(`/user/${id}`);
+        if (response.status === 200) {
+            return response.data;
+        }
+        throw new Error(`Failed to get current user with ID ${id}`);
     }
     catch (error) {
         console.log("get current user error:", error);
+        throw error;
     }
 };
