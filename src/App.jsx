25cd657bf8e49d84ef5686981bfa20ef9f52650b
Tala Hamad <128089053+talahamad1@users.noreply.github.com>
2023-05-12 12:40:26 +0300

    AR-63 create category component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Category from './components/ShopByCategory/ShopByCategory';
 function App() {
   const [count, setCount] = useState(0)
   return (
     <div className="App">
-      
+      <Category/>
       </div>
   )
 }
---
+++ src/components/ShopByCategory/ShopByCategory.jsx
@@ -0,0 +1,48 @@
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import ceramic from '../../../public/ceramic.jpg'
+import cloth from '../../../public/cloth.jpg';
+import jewel from '../../../public/jewel.jpg';
+import soap from '../../../public/soap (1).jpg';
+import home from '../../../public/home.jpg';
+import './ShopByCategory.css';
+
+function Category() {
+  return (
+    <div>
+      <Grid container spacing={1} columns={16} marginTop={0} paddingLeft={0}>
+        <Grid lg={8} md={8} sm={16} xs={16}>
+          <Link href="/ceramic" underline="none">
+            <img className='Image' src={ceramic} alt='Ceramic'/> 
+          </Link>
+        </Grid>
+        <Grid lg={8} md={8} sm={16} xs={16}>
+          <Link href="/Clothes" underline="none">
+            <img className='Image' src={cloth} alt='Clothes'/> 
+          </Link>
+        </Grid>
+      </Grid>
+
+      <div className='div'>
+        <Link href="/Accessories" underline="none">
+          <img className='Image1' src={jewel} alt='Accessories'/> 
+        </Link>
+      </div>
+
+      <Grid container spacing={1} columns={16} marginTop={0} paddingLeft={0}>
+        <Grid lg={8} md={8} sm={16} xs={16}>
+          <Link href="/Soap" underline="none">
+            <img className='Image' src={soap} alt='Soap'/> 
+          </Link>
+        </Grid>
+        <Grid lg={8} md={8} sm={16} xs={16}>
+          <Link href="/Home Decor" underline="none">
+            <img className='Image' src={home} alt='Home Decor'/> 
+          </Link>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default Category;
