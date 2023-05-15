9fe5bf3fe405498315d506995e84b875e128e3e9
Tala Hamad <128089053+talahamad1@users.noreply.github.com>
2023-05-15 15:49:19 +0300

    AR-76 create category page component

---
+++ src/components/Category/Ceramic/Ceramic.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Ceramic.css';
+
+
+
+function Ceramic (){
+
+
+return (
+    <div className='Category'>
+    <h1>  Ceramic </h1>
+
+<Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand > Filter product by : </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown title="Rate" id="basic-nav-dropdown" style={{ fontSize: '20px' }}>
+              <button >Hight to Low</button>
+              <button >Low to Hight</button>
+            </NavDropdown>
+
+            <NavDropdown title="Price" id="basic-nav-dropdown" style={{ fontSize: '20px' }}>
+            <button>Hight to Low</button>
+            <button>Low to Hight</button>
+            </NavDropdown>
+
+            <NavDropdown title="alphabetical order" id="basic-nav-dropdown" style={{ fontSize: '20px' }}>
+            <button>A - to -  Z</button>
+            <button>Z - to - A</button>
+            </NavDropdown>
+          </Nav></Navbar.Collapse>
+      </Container>
+    </Navbar>
+   <div className="Image">
+
+    <Container>
+      <Row>
+        <Col>-</Col>
+        <Col>-</Col>
+        <Col>-</Col>
+      </Row>
+      
+    </Container>
+  </div>
+</div>
+    
+  );
+};
+
+export default Ceramic;
