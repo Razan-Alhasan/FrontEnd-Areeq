6cffca5c2f074ee0cf09664624055da31441e74a
Razan Alhasan <r.a.alhassan@students.ptuk.edu.ps>
2023-05-20 20:10:15 +0300

    AR-79: create archive page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import ArchivePage from './components/ProductPage/Archive/Archive';
 
 function App() {
   return (
     <div className="App">
+      <ArchivePage/>
     </div>
 
   )
