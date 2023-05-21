d356c9b35d87a7a861cca43b8afbc2038233217d
Tala Hamad <128089053+talahamad1@users.noreply.github.com>
2023-05-21 22:39:55 +0300

    AR-63 create category component (#22)
    * AR-63 create category component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import AddProduct from './components/Product/AddProduct/AddProduct';
+
+import Category from './components/ShopByCategory/ShopByCategory';
+
 function App() {
   return (
     <div className="App">
 
-      < AddProduct/>
+      <Category/>
+
+
+     
 
 
 
