0754fd940c9c38645dad408367b86c60d60a530b
Tala Hamad <128089053+talahamad1@users.noreply.github.com>
2023-05-21 21:56:47 +0300

    AR-69 create add product component (#21)
    * AR-69 create add product component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import AddProduct from './components/Product/AddProduct/AddProduct';
 function App() {
   return (
     <div className="App">
+
+      < AddProduct/>
+
+
 
       </div>
   )
