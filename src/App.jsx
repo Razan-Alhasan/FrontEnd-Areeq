144a3216df4bf6baf8495864ef8a928f757fec9f
Razan Alhasan <115662722+Razan-Alhasan@users.noreply.github.com>
2023-05-21 17:26:09 +0300

    AR-83: create review page (#37)

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import Mynav from './components/DefaultLayout/Navbar/Navbar';
-import Footer from './components/DefaultLayout/Footer/Footer';
-
-import './App.css'
-
-
-
-
-
-
 
 function App() {
   return (
     <div className="App">
-
-      < Mynav />
-      
-      <Footer/>
-
-
-
-    
-
-      </div>
 
     </div>
 
