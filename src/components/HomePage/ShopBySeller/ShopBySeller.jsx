5208abf3d9c44231c5ace695874f115c4615a334
Tala Hamad <128089053+talahamad1@users.noreply.github.com>
2023-06-08 20:06:35 +0300

    fix shopBySeller (#54)

--- src/components/HomePage/ShopBySeller/ShopBySeller.jsx
+++ src/components/HomePage/ShopBySeller/ShopBySeller.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
 import Card from 'react-bootstrap/Card';
 import { getAllUsers } from '../../../api/userApi';
-import'./ShopBySeller.css';
+import './ShopBySeller.css';
 import { Link } from 'react-router-dom';
+
 const ShopBySeller = () => {
   const [sellers, setSellers] = useState([]);
 
@@ -16,26 +18,56 @@ const ShopBySeller = () => {
         console.error('Error fetching Sellers:', error);
       }
     }
+
     fetchSellers();
   }, []);
+
+  const CustomCarousel = ({ children }) => {
+    return (
+      <Carousel prevIcon={null} nextIcon={null} fade={true} interval={3000}>
+        {children}
+      </Carousel>
+    );
+  };
+
+  const chunkArray = (arr, size) => {
+    const chunkedArray = [];
+    for (let i = 0; i < arr.length; i += size) {
+      const chunk = arr.slice(i, i + size);
+      chunkedArray.push(chunk);
+    }
+    return chunkedArray;
+  };
+  if (sellers.length === 0) {
+    return null; // Render null or a loading indicator while fetching sellers
+  }
 
   return (
     <div className="ShopBySeller">
       <h2>Shop By Seller</h2>
-      <div className='card-flex row'>
-        {sellers && sellers.map((seller) => (
-          <Card key={seller.id} style={{ margin: '2%' }} className='col-md-3 col col-8'>
-            <Link to={`/FrontEnd-Areeq/seller/${seller.id}`}> 
-            <Card.Img variant='top' className='img-seller' style={{ padding: '5%', transition: '1s' }}
-              src={seller.image}
-            />
-             </Link> 
-            <Card.Body>
-              <Card.Title>{seller.name}</Card.Title>
-            </Card.Body>
-          </Card>
+      <CustomCarousel>
+        {chunkArray(sellers, 3).map((sellerChunk, index) => (
+          <Carousel.Item key={index}>
+            <div className="d-flex justify-content-around">
+              {sellerChunk.map((seller) => (
+                <Card key={seller.id} style={{ margin: '2%' }}>
+                  <Link to={`/FrontEnd-Areeq/seller/${seller.id}`}>
+                    <Card.Img
+                      variant="top"
+                      className="img-seller"
+                      style={{ padding: '5%', transition: '1s' }}
+                      src={seller.image}
+                    />
+                  </Link>
+                  <Card.Body>
+                    <Card.Title>{seller.name}</Card.Title>
+                  </Card.Body>
+                </Card>
+              ))}
+            </div>
+          </Carousel.Item>
         ))}
-      </div>
+      </CustomCarousel>
     </div>
   );
 };
